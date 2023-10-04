45c19cfbe51e62e2bcf4ff2bea0bb6affc02cc81
HarshaGotmare <145536633+HarshaGotmare@users.noreply.github.com>
2023-10-04 12:14:14 +0530

    Add files via upload

---
+++ continueDemo.java
@@ -0,0 +1,16 @@
+package com.java.harsha;
+
+public class continueDemo {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		for(int k=5; k<15; k++) {
+			if(k%2!=0)
+				continue;
+			System.out.println(k+ " ");
+		}
+
+	}
+
+}
